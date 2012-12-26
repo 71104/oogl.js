b84d18b8cb94d3ebb2e128b48096df6e853ab211
Alberto La Rocca <a71104@gmail.com>
2012-12-26 23:47:37 +0100

    ~

--- test/oogl-test.js
+++ test/oogl-test.js
@@ -439,6 +439,22 @@ OOGL.Vector2.prototype = {
 		var dot = this.x * n.x + this.y * n.y;
 		return new OOGL.Vector2(this.x - 2 * dot * n.x, this.y - 2 * dot * n.y);
 	},
+
+	/**
+	 * Refracts this vector given a surface normal `n` and the `eta` ratio
+	 * between refraction indices.
+	 *
+	 * This method modifies the original object.
+	 *
+	 * @method refract
+	 * @chainable
+	 * @param {OOGL.Vector2} n The surface normal at the incidence point.
+	 * @param {Number} eta The ratio between refraction indices.
+	 * @example
+	 *	var v = new OOGL.Vector2(0.3, -1);
+	 *	var n = new OOGL.Vector2(0, 1);
+	 *	v.refract(n, 1.3);
+	 */
 	refract: function (n, eta) {
 		var dot = this.x * n.x + this.y * n.y;
 		var k = 1 - eta * eta * (1 - dot * dot);
@@ -451,6 +467,21 @@ OOGL.Vector2.prototype = {
 		}
 		return this;
 	},
+
+	/**
+	 * Refracts this vector given a surface normal `n` and the `eta` ratio
+	 * between refraction indices; the computed vector is returned as a new
+	 * `OOGL.Vector2` object, this object is not changed.
+	 *
+	 * @method getRefracted
+	 * @param {OOGL.Vector2} n The surface normal at the incidence point.
+	 * @param {Number} eta The ratio between refraction indices.
+	 * @return {OOGL.Vector2} The refracted vector.
+	 * @example
+	 *	var v = new OOGL.Vector2(0.3, -1);
+	 *	var n = new OOGL.Vector2(0, 1);
+	 *	var w = v.getRefracted(n, 1.3);
+	 */
 	getRefracted: function (n, eta) {
 		var dot = this.x * n.x + this.y * n.y;
 		var k = 1 - eta * eta * (1 - dot * dot);
@@ -494,9 +525,40 @@ OOGL.Vector2.J = new OOGL.Vector2(0, 1);
 
 /*global OOGL: false */
 
+/**
+ * A mutable 3-component vector.
+ *
+ * @class OOGL.Vector3
+ * @constructor
+ * @param {Number} x The X component.
+ * @param {Number} y The Y component.
+ * @param {Number} z The Z component.
+ * @example
+ *	var v = new OOGL.Vector3(3, 4, 5);
+ */
 OOGL.Vector3 = function (x, y, z) {
+	/**
+	 * The X component.
+	 *
+	 * @property x
+	 * @type Number
+	 */
 	this.x = x;
+
+	/**
+	 * The Y component.
+	 *
+	 * @property y
+	 * @type Number
+	 */
 	this.y = y;
+
+	/**
+	 * The Z component.
+	 *
+	 * @property z
+	 * @type Number
+	 */
 	this.z = z;
 };
 
@@ -1335,6 +1397,7 @@ OOGL.Matrix3.IDENTITY = new OOGL.Matrix3([1, 0, 0, 0, 1, 0, 0, 0, 1]);
  * @param {Number} x The X component of the rotation axis.
  * @param {Number} y The Y component of the rotation axis.
  * @param {Number} z The Z component of the rotation axis.
+ * @param {Number} a The rotation angle.
  * @example
  *	var m = new OOGL.RotationMatrix3(0, 1, 0, Math.PI / 2); // 90 degrees horizontal rotation
  */
@@ -1378,9 +1441,18 @@ OOGL.ScalingMatrix3 = function (x, y, z) {
  *
  * @class OOGL.Matrix4
  * @constructor
- * @param {Number[]} data TODO
+ * @param {Number[]} data A 16-element array of the floating point values to be
+ *	put into the matrix.
+ *
+ * Matrix elements are specified in row-major order.
+ *
+ * The specified `data` array is duplicated into the matrix, changes to it will
+ * not affect the content of the matrix.
+ *
+ * An exception is thrown if the length of the `data` array is not 16.
  * @example
- *	TODO
+ *	// create the 4x4 identity matrix
+ *	var matrix = new OOGL.Matrix4([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
  */
 OOGL.Matrix4 = function (data) {
 	if (data.length != 16) {
@@ -1391,40 +1463,85 @@ OOGL.Matrix4 = function (data) {
 
 OOGL.Matrix4.prototype = {
 	/**
-	 * TODO
+	 * Returns the element at the specified row and column in the matrix.
+	 *
+	 * Row and column indices are zero-based. This method is equivalent to
+	 * fetching the `i * 4 + j`-th element of the array:
+	 *
+	 *	matrix.get(i, j) == matrix[i * 4 + j] // true
 	 *
 	 * @method get
-	 * @param {Number} i TODO
-	 * @param {Number} j TODO
-	 * @return {Number} TODO
+	 * @param {Number} i The row index.
+	 * @param {Number} j The column index.
+	 * @return {Number} The value at the specified row and column.
 	 * @example
-	 *	TODO
+	 *	var m = new OOGL.Matrix4([1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]);
+	 *	if (m.get(2, 3) == m[4]) { // true
+	 *		...
 	 */
 	get: function (i, j) {
 		return this[i * 4 + j];
 	},
 
 	/**
-	 * TODO
+	 * Changes the element at the specified row and column in the matrix.
+	 *
+	 * Row and column indices are zero-based. This method is equivalent to
+	 * setting the `i * 4 + j`-th element of the array:
+	 *
+	 *	matrix.put(i, j, x);
+	 *	matrix[i * 4 + j] = x; // same as previous
 	 *
 	 * @method put
-	 * @param {Number} i TODO
-	 * @param {Number} j TODO
-	 * @param {Number} value TODO
+	 * @param {Number} i The row index.
+	 * @param {Number} j The column index.
+	 * @param {Number} value The value to put at the specified row and column.
 	 * @example
-	 *	TODO
+	 *	var matrix = new OOGL.Matrix4([0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]);
+	 *	matrix.put(0, 0, 3); // now matrix is [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 	 */
 	put: function (i, j, value) {
 		this[i * 4 + j] = value;
 		return this;
 	}
-	// TODO
 };
 
+/**
+ * Creates a 3D translation matrix.
+ *
+ * @class OOGL.TranslationMatrix4
+ * @extends OOGL.Matrix4
+ * @constructor
+ * @param {Number} x The X component of the translation vector.
+ * @param {Number} y The Y component of the translation vector.
+ * @param {Number} z The Z component of the translation vector.
+ * @example
+ *	// translate by 3 units on the X axis, 4 on the Y axis and 5 on the Z axis
+ *	var m = new OOGL.TranslationMatrix4(3, 4, 5);
+ */
 OOGL.TranslationMatrix4 = function (x, y, z) {
 	return new OOGL.Matrix4([1, 0, 0, x, 0, 1, 0, y, 0, 0, 1, z, 0, 0, 0, 1]);
 };
 
+/**
+ * Creates a 3D rotation matrix that rotates everything counterclockwise about
+ * the specified `(x, y, z)` axis by the specified `a` angle.
+ *
+ * The specified `x`, `y` and `z` components must form a unit-length vector.
+ *
+ * The created matrix is identical to a rotation matrix created by using
+ * `OOGL.RotationMatrix3` and converted using its `toHomogeneous` method.
+ *
+ * @class OOGL.RotationMatrix4
+ * @extends OOGL.Matrix4
+ * @constructor
+ * @param {Number} x The X component of the rotation axis.
+ * @param {Number} y The Y component of the rotation axis.
+ * @param {Number} z The Z component of the rotation axis.
+ * @param {Number} a The rotation angle.
+ * @example
+ *	var m = new OOGL.RotationMatrix4(0, 1, 0, Math.PI / 2); // 90 degrees horizontal rotation
+ */
 OOGL.RotationMatrix4 = function (x, y, z, a) {
 	var s = Math.sin(a);
 	var c = Math.cos(a);
@@ -1448,6 +1565,22 @@ OOGL.RotationMatrix4 = function (x, y, z, a) {
 	]);
 };
 
+/**
+ * Creates a 3D scaling matrix using the specified `x`, `y` and `z` scaling
+ * factors.
+ *
+ * The created matrix is identical to a scaling matrix created by using
+ * `OOGL.ScalingMatrix3` and converted using its `toHomogeneous` method.
+ *
+ * @class OOGL.ScalingMatrix4
+ * @extends OOGL.Matrix4
+ * @constructor
+ * @param {Number} x The X scaling factor.
+ * @param {Number} y The Y scaling factor.
+ * @param {Number} z The Z scaling factor.
+ * @example
+ *	var m = new OOGL.ScalingMatrix4(0.5, 0.5, 0.5); // halves the size of everything
+ */
 OOGL.ScalingMatrix4 = function (x, y, z) {
 	return new OOGL.Matrix4([x, 0, 0, 0, 0, y, 0, 0, 0, 0, z, 0, 0, 0, 0, 1]);
 };
@@ -2711,6 +2844,18 @@ context.Textures = function (textures) {
  */
 context.Shader = function (type) {
 	var shader = context.createShader(type);
+
+	/**
+	 * Queries a shader-related parameter.
+	 *
+	 * `gl.getShaderParameter` equivalent.
+	 *
+	 * @method getParameter
+	 * @param {String} name TODO
+	 * @return {Mixed} TODO
+	 * @example
+	 *	TODO
+	 */
 	shader.getParameter = function (name) {
 		return context.getShaderParameter(shader, name);
 	};
