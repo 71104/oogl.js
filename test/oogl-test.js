fd362386cc2e6ea72123c28e8563279d727c52ef
Alberto La Rocca <a71104@gmail.com>
2012-12-27 16:38:36 +0100

    ~

--- test/oogl-test.js
+++ test/oogl-test.js
@@ -1,4 +1,4 @@
-/*! Object-Oriented Graphics Library - v1.0.0 - 2012-12-26
+/*! Object-Oriented Graphics Library - v1.0.0 - 2012-12-27
 * Released under the MIT License
 * http://oogljs.com/
 * Copyright (c) 2012 Alberto La Rocca */
@@ -675,10 +675,50 @@ OOGL.Vector3.K = new OOGL.Vector3(0, 0, 1);
 
 /*global OOGL: false */
 
+/**
+ * A mutable 4-component vector. This is usually the homogeneous version of a
+ * 3-component vector.
+ *
+ * @class OOGL.Vector4
+ * @constructor
+ * @param {Number} x The X component.
+ * @param {Number} y The Y component.
+ * @param {Number} z The Z component.
+ * @param {Number} w The homogeneous W component.
+ * @example
+ *	var v = new OOGL.Vector4(3, 4, 5, 1);
+ */
 OOGL.Vector4 = function (x, y, z, w) {
+	/**
+	 * The X component.
+	 *
+	 * @property x
+	 * @type Number
+	 */
 	this.x = x;
+
+	/**
+	 * The y component.
+	 *
+	 * @property y
+	 * @type Number
+	 */
 	this.y = y;
+
+	/**
+	 * The Z component.
+	 *
+	 * @property z
+	 * @type Number
+	 */
 	this.z = z;
+
+	/**
+	 * The homogeneous W component.
+	 *
+	 * @property w
+	 * @type Number
+	 */
 	this.w = w;
 };
 
