dfe7e5d2718092e5a26c0a656fbfdabc9a79073f
Alberto La Rocca <a71104@gmail.com>
2012-12-27 16:37:49 +0100

    ~

--- src/Vector4.js
+++ src/Vector4.js
@@ -1,9 +1,49 @@
 /*global OOGL: false */
 
+/**
+ * A mutable 4-component vector. This is usually the homogeneous version of a
+ * 3-component vector.
+ *
+ * @class OOGL.Vector4
+ * @constructor
+ * @param {Number} x The X component.
+ * @param {Number} y The Y component.
+ * @param {Number} z The Z component.
+ * @param {Number} w The homogeneous W component.
+ * @example
+ *	var v = new OOGL.Vector4(3, 4, 5, 1);
+ */
 OOGL.Vector4 = function (x, y, z, w) {
+	/**
+	 * The X component.
+	 *
+	 * @property x
+	 * @type Number
+	 */
 	this.x = x;
+
+	/**
+	 * The y component.
+	 *
+	 * @property y
+	 * @type Number
+	 */
 	this.y = y;
+
+	/**
+	 * The Z component.
+	 *
+	 * @property z
+	 * @type Number
+	 */
 	this.z = z;
+
+	/**
+	 * The homogeneous W component.
+	 *
+	 * @property w
+	 * @type Number
+	 */
 	this.w = w;
 };
 
