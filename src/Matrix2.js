17f628a691c5e97395efe25ce44948dd35bb37f0
Alberto La Rocca <a71104@gmail.com>
2012-12-31 02:46:28 +0100

    ~

--- src/Matrix2.js
+++ src/Matrix2.js
@@ -4,12 +4,11 @@
  * A mutable 2x2 matrix.
  *
  * @class OOGL.Matrix2
- * @extends Array
  * @constructor
  * @param {Number[]} data A 4-element array of the floating point values to be
  *	put into the matrix.
  *
- * Matrix elements are specified in row-major order.
+ * Matrix elements are specified in column-major order.
  *
  * The specified `data` array is duplicated into the matrix, changes to it will
  * not affect the content of the matrix.
@@ -22,7 +21,45 @@ OOGL.Matrix2 = function (data) {
 	if (data.length != 4) {
 		throw 'A 2x2 matrix must have exactly 4 elements.';
 	}
-	return data.slice(0);
+
+	/**
+	 * The element at cell (0, 0) of this matrix.
+	 *
+	 * @property 0
+	 * @type Number
+	 * @example
+	 *	var a00 = matrix[0];
+	 */
+
+	/**
+	 * The element at cell (1, 0) of this matrix.
+	 *
+	 * @property 1
+	 * @type Number
+	 * @example
+	 *	var a10 = matrix[1];
+	 */
+
+	/**
+	 * The element at cell (0, 1) of this matrix.
+	 *
+	 * @property 2
+	 * @type Number
+	 * @example
+	 *	var a01 = matrix[2];
+	 */
+
+	/**
+	 * The element at cell (1, 1) of this matrix.
+	 *
+	 * @property 3
+	 * @type Number
+	 * @example
+	 *	var a11 = matrix[3];
+	 */
+	for (var i = 0; i < 4; i++) {
+		this[i] = data[i];
+	}
 };
 
 OOGL.Matrix2.prototype = {
@@ -30,9 +67,9 @@ OOGL.Matrix2.prototype = {
 	 * Returns the element at the specified row and column in the matrix.
 	 *
 	 * Row and column indices are zero-based. This method is equivalent to
-	 * fetching the `i * 2 + j`-th element of the array:
+	 * fetching the `j * 2 + i`-th element of the array:
 	 *
-	 *	matrix.get(i, j) == matrix[i * 2 + j] // true
+	 *	matrix.get(i, j) == matrix[j * 2 + i] // true
 	 *
 	 * @method get
 	 * @param {Number} i The row index.
@@ -40,20 +77,20 @@ OOGL.Matrix2.prototype = {
 	 * @return {Number} The value at the specified row and column.
 	 * @example
 	 *	var m = new OOGL.Matrix2([1, 2, 3, 4]);
-	 *	var determinant = m.get(0, 0) * m.get(1, 1) - m.get(0, 1) * m.get(1, 0);
+	 *	var determinant = m.get(0, 0) * m.get(1, 1) - m.get(0, 1) * m.get(1, 0); // -5
 	 */
 	get: function (i, j) {
-		return this[i * 2 + j];
+		return this[j * 2 + i];
 	},
 
 	/**
 	 * Changes the element at the specified row and column in the matrix.
 	 *
 	 * Row and column indices are zero-based. This method is equivalent to
-	 * setting the `i * 2 + j`-th element of the array:
+	 * setting the `j * 2 + i`-th element of the array:
 	 *
 	 *	matrix.put(i, j, x);
-	 *	matrix[i * 2 + j] = x; // same as previous
+	 *	matrix[j * 2 + i] = x; // same as previous
 	 *
 	 * @method put
 	 * @param {Number} i The row index.
@@ -61,10 +98,10 @@ OOGL.Matrix2.prototype = {
 	 * @param {Number} value The value to put at the specified row and column.
 	 * @example
 	 *	var matrix = new OOGL.Matrix2([3, 3, 0, 3]);
-	 *	matrix.put(1, 0, 3); // now matrix is [3, 3, 3, 3]
+	 *	matrix.put(0, 1, 3); // now matrix is [3, 3, 3, 3]
 	 */
 	put: function (i, j, value) {
-		this[i * 2 + j] = value;
+		this[j * 2 + i] = value;
 		return this;
 	},
 
@@ -222,18 +259,39 @@ OOGL.Matrix2.prototype = {
 	 *	var v = new OOGL.Vector2(2, 2);
 	 *	var w = m.by(v); // (4, 4)
 	 */
+
+	/**
+	 * Left-multiplies this matrix by the specified `Matrix2` object and returns
+	 * the product as a new `Matrix2` object. Neither this matrix nor the
+	 * specified one are changed.
+	 *
+	 * @method by
+	 * @param {OOGL.Matrix2} v The matrix to multiply.
+	 * @return {OOGL.Matrix2} The product matrix.
+	 * @example
+	 *	var m1 = new OOGL.Matrix2([2, 0, 0, 2]);
+	 *	var m2 = new OOGL.Matrix2([1, 2, 3, 4]);
+	 *	var m3 = m1.by(m2); // (2, 4, 6, 8)
+	 */
 	by: function (x) {
-		if (typeof x === 'number') {
+		if (x instanceof OOGL.Vector2) {
+			return new OOGL.Vector2(
+				this[0] * x.x + this[2] * x.y,
+				this[1] * x.x + this[3] * x.y
+				);
+		} else if (x instanceof OOGL.Matrix2) {
+			return new OOGL.Matrix2([
+				this[0] * x[0] + this[2] * x[1],
+				this[1] * x[0] + this[3] * x[1],
+				this[0] * x[2] + this[2] * x[3],
+				this[1] * x[2] + this[3] * x[3]
+			]);
+		} else {
 			var newArray = [];
 			for (var i = 0; i < 4; i++) {
 				newArray.push(this[i] * x);
 			}
 			return new OOGL.Matrix2(newArray);
-		} else {
-			return new OOGL.Vector2(
-				this[0] * x.x + this[1] * x.y,
-				this[2] * x.x + this[3] * x.y
-				);
 		}
 	},
 
@@ -244,10 +302,10 @@ OOGL.Matrix2.prototype = {
 	 * @return {Number} The computed determinant.
 	 * @example
 	 *	var m = new OOGL.Matrix2([1, 2, 3, 4]);
-	 *	var d = m.determinant(); // -2
+	 *	var d = m.determinant(); // -5
 	 */
 	determinant: function () {
-		return this[0] * this[1] - this[2] * this[3];
+		return this[0] * this[3] - this[1] * this[2];
 	},
 
 	/**
@@ -257,14 +315,14 @@ OOGL.Matrix2.prototype = {
 	 * @chainable
 	 * @example
 	 *	var matrix = new OOGL.Matrix2([1, 2, 3, 4]);
-	 *	matrix.invert(); // matrix is now [-2, -1, -1.5, -0.5]
+	 *	matrix.invert(); // matrix is now [-0.8, -0.6, -0.4, -0.2]
 	 */
 	invert: function () {
-		var determinant = this[0] * this[1] - this[2] * this[3];
+		var determinant = this[0] * this[3] - this[1] * this[2];
 		var newArray = [
 			this[3] / determinant,
-			this[1] / determinant,
 			this[2] / determinant,
+			this[1] / determinant,
 			this[0] / determinant
 		];
 		for (var i = 0; i < 4; i++) {
@@ -281,14 +339,14 @@ OOGL.Matrix2.prototype = {
 	 * @return {OOGL.Matrix2} The inverse matrix.
 	 * @example
 	 *	var m1 = new OOGL.Matrix2([1, 2, 3, 4]);
-	 *	var m2 = m1.getInverse(); // [-2, -1, -1.5, -0.5]
+	 *	var m2 = m1.getInverse(); // [-0.8, -0.6, -0.4, -0.2]
 	 */
 	getInverse: function () {
-		var determinant = this[0] * this[1] - this[2] * this[3];
+		var determinant = this[0] * this[3] - this[1] * this[2];
 		return new OOGL.Matrix2([
 			this[3] / determinant,
-			this[1] / determinant,
 			this[2] / determinant,
+			this[1] / determinant,
 			this[0] / determinant
 		]);
 	}
@@ -325,7 +383,7 @@ OOGL.Matrix2.IDENTITY = new OOGL.Matrix2([1, 0, 0, 1]);
 OOGL.RotationMatrix2 = function (a) {
 	var s = Math.sin(a);
 	var c = Math.cos(a);
-	return new OOGL.Matrix2([c, -s, s, c]);
+	return new OOGL.Matrix2([c, s, -s, c]);
 };
 
 /**
