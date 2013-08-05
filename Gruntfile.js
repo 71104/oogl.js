e7a95bb7b9dd6e1c356204c5a0308f5502ad6ac6
Alberto La Rocca <a71104@gmail.com>
2013-08-05 15:27:42 +0200

    Adding the clean task

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,6 +33,10 @@ module.exports = function (grunt) {
 				'* Copyright (c) <%= grunt.template.today("yyyy") %> Alberto La Rocca */\n'
 		},
 
+		clean: {
+			dist: ['bin']
+		},
+
 		jshint: {
 			options: {
 				camelcase: true,
@@ -121,6 +125,7 @@ module.exports = function (grunt) {
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-concat');
