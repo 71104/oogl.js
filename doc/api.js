33543242e0afe35e58f1368b135161ade51527bf
Alberto La Rocca <a71104@gmail.com>
2013-08-14 11:16:12 +0200

    ~

--- doc/api.js
+++ doc/api.js
@@ -4,6 +4,7 @@ YUI.add("yuidoc-meta", function(Y) {
         "OOGL.Ajax",
         "OOGL.Context",
         "OOGL.IsometricProjection",
+        "OOGL.Loader",
         "OOGL.Matrix2",
         "OOGL.Matrix3",
         "OOGL.Matrix4",
@@ -16,14 +17,22 @@ YUI.add("yuidoc-meta", function(Y) {
         "OOGL.ScalingMatrix2",
         "OOGL.ScalingMatrix3",
         "OOGL.ScalingMatrix4",
+        "OOGL.Timing",
         "OOGL.TranslationMatrix4",
         "OOGL.Vector2",
         "OOGL.Vector3",
         "OOGL.Vector4",
+        "OOGL.XRotationMatrix3",
+        "OOGL.XRotationMatrix4",
+        "OOGL.YRotationMatrix3",
+        "OOGL.YRotationMatrix4",
+        "OOGL.ZRotationMatrix3",
+        "OOGL.ZRotationMatrix4",
         "context.AjaxFragmentShader",
         "context.AjaxProgram",
         "context.AjaxVertexShader",
         "context.ArrayBuffer",
+        "context.AsyncCubeMap",
         "context.AsyncTexture",
         "context.AttributeArray1",
         "context.AttributeArray2",
@@ -63,12 +72,12 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "context",
             "name": "context",
-            "description": "TODO"
+            "description": "This is actually a _pseudo_-module used to document OOGL classes whose\nnamespace is a WebGL/OOGL context instance.\n\nYou can get a `context` object by invoking the `OOGL.Context` constructor:\n\n\tvar context = new OOGL.Context(canvas);\n\nYou can then use that object to construct context-specific objects, such as\n`Texture2D` objects:\n\n\tvar texture = new context.Texture2D();\n\nThe `context` object is sometimes referred to with the `oogl` name because it\nis a normal `WebGLContext` object containing all the standard WebGL features,\nsuch as `createTexture`, `createShader`, `flush` and so on, plus\nOOGL-specific features such as `Texture2D`."
         },
         {
             "displayName": "OOGL",
             "name": "OOGL",
-            "description": "This is OOGL's main namespace object and contains all the OOGL classes.\n\nAll OOGL-specific class constructors are contained in this object. For\nexample, to create an `OOGL.RenderLoop` object you construct it in this way:\n\n\tvar loop = new OOGL.RenderLoop( ... );\n\nThe `OOGL` object may also be invoked as a function, in which it expectes\nexactly one function argument which is a user-defined callback function that\ngets invoked as soon as the DOM of the page has loaded (this is accomplished\nusing the `DOMCOntentLoaded` JavaScript event):\n\n\tOOGL(function () {\n\t\t// here the DOM has fully loaded\n\t});\n\nThis is typically the right place to put WebGL/OOGL initialization code, such\nas GLSL shader and other asset loading code, as well as GLSL program\ncompilation and linking."
+            "description": "This is OOGL's main namespace object and contains all the OOGL classes.\n\nSome OOGL-specific class constructors are contained in this object. For\nexample, to create an `OOGL.RenderLoop` object you construct it in this way:\n\n\tvar loop = new OOGL.RenderLoop( ... );\n\nThe `OOGL` object may also be invoked as a function, in which it expectes\nexactly one function argument which is a user-defined callback function that\ngets invoked as soon as the DOM of the page has loaded (this is accomplished\nusing the `DOMCOntentLoaded` JavaScript event):\n\n\tOOGL(function () {\n\t\t// here the DOM has fully loaded\n\t});\n\nThis is typically the right place to put WebGL/OOGL initialization code, such\nas GLSL shader and other asset loading code, as well as GLSL program\ncompilation and linking."
         }
     ]
 } };
