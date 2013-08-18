96c51064fb9fccee4aff9620f71aa8e37ea53405
Alberto La Rocca <a71104@gmail.com>
2013-08-18 22:56:59 +0200

    Bugfix

--- src/Arrays.js
+++ src/Arrays.js
@@ -956,7 +956,7 @@ context.AttributeArrays = function (count) {
 		 *	arrays.add4b([1, 2, 3, 4, -1, -2, -3, -4]);
 		 */
 		add4b: function (data, normalize) {
-			arrays.push(new context.AttributeArray3(arrays.length, 'byte', data, normalize));
+			arrays.push(new context.AttributeArray4(arrays.length, 'byte', data, normalize));
 		},
 
 		/**
@@ -971,7 +971,7 @@ context.AttributeArrays = function (count) {
 		 *	arrays.add4ub([1, 2, 3, 4, 5, 6, 7, 8]);
 		 */
 		add4ub: function (data, normalize) {
-			arrays.push(new context.AttributeArray3(arrays.length, 'ubyte', data, normalize));
+			arrays.push(new context.AttributeArray4(arrays.length, 'ubyte', data, normalize));
 		},
 
 		/**
@@ -987,7 +987,7 @@ context.AttributeArrays = function (count) {
 		 *	arrays.add4s([1, 2, 3, 4, -5, -6, -7, -8]);
 		 */
 		add4s: function (data, normalize) {
-			arrays.push(new context.AttributeArray3(arrays.length, 'short', data, normalize));
+			arrays.push(new context.AttributeArray4(arrays.length, 'short', data, normalize));
 		},
 
 		/**
@@ -1003,7 +1003,7 @@ context.AttributeArrays = function (count) {
 		 *	arrays.add4us([1, 2, 3, 4, 5, 6, 7, 8]);
 		 */
 		add4us: function (data, normalize) {
-			arrays.push(new context.AttributeArray3(arrays.length, 'ushort', data, normalize));
+			arrays.push(new context.AttributeArray4(arrays.length, 'ushort', data, normalize));
 		},
 
 		/**
@@ -1019,7 +1019,7 @@ context.AttributeArrays = function (count) {
 		 *	arrays.add4f([1, 2, 3, 4, 5, 6, 7, 8]);
 		 */
 		add4f: function (data, normalize) {
-			arrays.push(new context.AttributeArray3(arrays.length, 'float', data, normalize));
+			arrays.push(new context.AttributeArray4(arrays.length, 'float', data, normalize));
 		},
 
 		/**
